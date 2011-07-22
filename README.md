cee0461c40cb895235f94779e094f12fe4c10da5
Shane Perreault <shaneprrlt@gmail.com>
2011-07-22 18:37:41 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#MultiDB#
+
+MultiDB is an incredibly small handler for using Multiple Databases across an application. It allows you to easily Create, Read, Update, and Delete Databases from your Application at will.
---
+++ src/database.php
@@ -0,0 +1,50 @@
+<?php
+
+/*	Created by Shane Perreault
+ *	Friday July 22, 2011
+ *
+ *	MultiDB - PHP Tool
+ *	Multiple Database Handling w/ CRUD
+*/
+
+class Database extends PDO
+{
+
+	// Information
+	protected static $_db_info = array();
+
+	// Instances
+	protected static $_db_instances = array();
+	
+	public static function createDB($host, $user, $pass, $db_name) {
+	
+		if(empty(self::$_db_instances[strtolower($db_name)]))
+		{
+			self::$_db_info[strtolower($db_name)]['host'] = $host;
+			self::$_db_info[strtolower($db_name)]['user'] = $user;
+			self::$_db_info[strtolower($db_name)]['pass'] = $pass;
+			self::$_db_info[strtolower($db_name)]['name'] = $db_name;
+			
+			$dsn = "mysql:host=" . self::$_db_info[strtolower($db_name)]['host'] . 
+				   ";dbname=" . self::$_db_info[strtolower($db_name)]['name'];
+				   
+			self::_$db_instances[strtolower($db_name)] = new Database($dsn, self::$_db_info[strtolower($db_name)]['user'], 
+																			self::$_db_info[strtolower($db_name)]['pass']);
+		}
+	}
+	
+	public static function getDB($db_name) {
+		
+		if(!empty(self::$_db_instances[strtolower($db_name)]))
+			return self::$_db_instances[strtolower($db_name)];
+	}
+	
+	
+	public static function deleteDB($db_name) {
+	
+		if(!empty(self::$_db_instances[strtolower($db_name)])) {
+			unset(self::$_db_instances[strtolower($db_name)]);
+		}
+	}
+
+}
